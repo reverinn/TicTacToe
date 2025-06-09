af78a58fa7ae9df6045720a5316307c2e6b6d205
reverinn <almatelinfo@gmail.com>
2025-06-10 00:11:28 +0200

    reso quasi del tutto funzionante

--- src/main/java/Package/Main.java
+++ src/main/java/Package/Main.java
@@ -1,23 +1,33 @@
 package Package;
 import Exception.PosizioneException;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String[][] map = new String[3][3];
         TicTacToe game = new TicTacToe();
+        char scelta;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 map[i][j] = "|";
             }
         }
-        game.stampaMap(map);
-        while (true) {
-            try {
-                game.selezionaPosizione(map);
-            } catch (PosizioneException e) {
-                System.out.println(e.getMessage());
-            }
+        do{
+            System.out.print("Vuoi Giocare? (scrivi n per terminare il gioco): ");
+            scelta = sc.next().charAt(0);
+            if (scelta == 'n') { break; }
             game.stampaMap(map);
-        }
+            while (!game.controllaVittoria(map)) {
+                try {
+                    game.selezionaPosizione(map);
+                } catch (PosizioneException e) {
+                    System.out.println(e.getMessage());
+                }
+                game.stampaMap(map);
+            }
+            game.resetMap(map);
+        }while (scelta != 'n');
     }
 }
--- src/main/java/Package/TicTacToe.java
+++ src/main/java/Package/TicTacToe.java
@@ -10,8 +10,26 @@ public class TicTacToe {
     private int posizioneColonnaX = 0;
     private int posizioneRigaO = 0;
     private int posizioneColonnaO = 0;
+    private int puntiX = 0;
+    private int puntiO = 0;
 
     public TicTacToe() {}
+
+    public void setPuntiX(int puntiX){
+        this.puntiX = puntiX;
+    }
+
+    public int getPuntiX(){
+        return puntiX;
+    }
+
+    public void setPuntiO(int puntiO){
+        this.puntiO = puntiO;
+    }
+
+    public int getPuntiO(){
+        return puntiO;
+    }
 
     public void updateTurno(){
         turno++;
@@ -25,7 +43,7 @@ public class TicTacToe {
         for(int i = 0; i < 3; i++){
             System.out.print("\n");
             for(int j = 0; j < 3; j++){
-                System.out.print(map[i][j]);
+                System.out.print(map[i][j] + "\t");
             }
         }
     }
@@ -67,4 +85,93 @@ public class TicTacToe {
         return false;
     }
 
+    public void vittoriaX(){
+        System.out.println("Tris! Ha vinto la X!");
+        setPuntiX(getPuntiX() + 1);
+    }
+
+    public void vittoriaO(){
+        System.out.println("Tris! Ha vinto il O!");
+        setPuntiO((getPuntiO()) + 1);
+    }
+
+
+    public boolean controllaRighe(String[][] map){
+        if (map[0][0].equals(map[0][1]) && map[0][1].equals(map[0][2]) && !map[0][0].equals("|")){
+            if (map[0][0].equals("X")){
+                vittoriaX();
+            }
+            else if (map[0][0].equals("O")){
+                vittoriaO();
+            }
+            return true;
+        }
+        else if (map[1][0].equals(map[1][1]) && map[1][1].equals(map[1][2]) && !map[1][0].equals("|")){
+            if (map[1][0].equals("X")){
+                vittoriaX();
+            }
+            else if (map[1][0].equals("O")){
+                vittoriaO();
+            }
+            return true;
+        }
+        else if (map[2][0].equals(map[2][1]) && map[2][1].equals(map[2][2]) && !map[2][0].equals("|")){
+            if (map[2][0].equals("X")){
+                vittoriaX();
+            }
+            else if (map[2][0].equals("O")){
+                vittoriaO();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean controllaColonne(String[][] map){
+        if (map[0][0].equals(map[1][0]) && map[1][0].equals(map[2][0]) && !map[0][0].equals("|")){
+            if (map[0][0].equals("X")){
+                vittoriaX();
+            }
+            else if (map[0][0].equals("O")){
+                vittoriaO();
+            }
+            return true;
+        }
+        else if (map[0][1].equals(map[1][1]) && map[1][1].equals(map[2][1]) && !map[0][1].equals("|")){
+            if (map[0][1].equals("X")){
+                vittoriaX();
+            }
+            else if (map[0][1].equals("O")){
+                vittoriaO();
+            }
+            return true;
+        }
+        else if (map[0][2].equals(map[1][2]) && map[1][2].equals(map[2][2]) && !map[0][2].equals("|")){
+            if (map[0][2].equals("X")){
+                vittoriaX();
+            }
+            else if (map[0][2].equals("O")){
+                vittoriaO();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean controllaVittoria(String[][] map){
+        if (controllaRighe(map) || controllaColonne(map)) {
+            System.out.println("Gioco Finito!");
+            System.out.println("Classifiche: \nX = " + getPuntiX() + "\nO = " + getPuntiO());
+            return true;
+        }
+        return false;
+    }
+
+    public void resetMap(String[][] map){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                map[i][j] = "|";
+            }
+        }
+    }
 }
